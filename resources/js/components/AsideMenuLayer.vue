2ad9a59d170eaa6db558ab3d591477884cce8a66
teguhwaluyojati <teguhwaluyojati14@gmail.com>
2025-10-21 15:40:16 +0700

    Fixing Menu & Aside Menu

--- resources/js/components/AsideMenuLayer.vue
+++ resources/js/components/AsideMenuLayer.vue
@@ -35,7 +35,7 @@ const asideLgCloseClick = (event) => {
     id="aside"
     class="lg:py-2 lg:pl-2 w-60 fixed flex z-40 top-0 h-screen transition-position overflow-hidden"
   >
-    <div class="aside lg:rounded-2xl flex-1 flex flex-col overflow-hidden dark:bg-slate-900">
+    <div class="aside lg:rounded-2xl flex-1 flex flex-col overflow-hidden bg-white dark:bg-slate-900">
       <div class="aside-brand flex flex-row h-14 items-center justify-between dark:bg-slate-900">
         <div class="text-center flex-1 lg:text-left lg:pl-6 xl:text-center xl:pl-0">
           <b class="font-black">One</b>
@@ -45,7 +45,7 @@ const asideLgCloseClick = (event) => {
         </button>
       </div>
       <div
-        class="flex-1 overflow-y-auto overflow-x-hidden aside-scrollbars dark:aside-scrollbars-[slate]"
+        class="flex-1 overflow-y-auto overflow-x-hidden aside-scrollbars dark:aside-scrollbars-[slate]-700"
       >
         <AsideMenuList :menu="menu" @menu-click="menuClick" />
       </div>
--- resources/js/menuNavBar.js
+++ resources/js/menuNavBar.js
@@ -8,7 +8,6 @@ import {
   mdiEmail,
   mdiLogout,
   mdiThemeLightDark,
-  mdiGithub,
   mdiReact,
 } from '@mdi/js'
 
@@ -66,20 +65,7 @@ export default [
     isDesktopNoLabel: true,
     isToggleLightDark: true,
   },
-  {
-    icon: mdiGithub,
-    label: 'GitHub',
-    isDesktopNoLabel: true,
-    href: 'https://github.com/justboil/admin-one-vue-tailwind',
-    target: '_blank',
-  },
-  {
-    icon: mdiReact,
-    label: 'React version',
-    isDesktopNoLabel: true,
-    href: 'https://github.com/justboil/admin-one-react-tailwind',
-    target: '_blank',
-  },
+
   {
     icon: mdiLogout,
     label: 'Log out',
