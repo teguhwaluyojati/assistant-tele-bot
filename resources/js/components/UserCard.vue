ba5b190cd4d52ba8921f820f6f18b5a186b2785b
teguhwaluyojati <teguhwaluyojati14@gmail.com>
2025-11-21 14:34:34 +0700

    Click to Preview Foto Profile

--- resources/js/components/UserCard.vue
+++ resources/js/components/UserCard.vue
@@ -11,6 +11,17 @@ import PillTag from '@/components/PillTag.vue'
 const mainStore = useMainStore()
 
 const userName = computed(() => mainStore.userName)
+const userAvatar = computed(() => mainStore.userAvatar)
+
+const isModalActive = ref(false)
+
+const openModal = () => {
+  isModalActive.value = true
+}
+
+const closeModal = () => {
+  isModalActive.value = false
+}
 
 const userSwitchVal = ref(false)
 </script>
@@ -18,7 +29,9 @@ const userSwitchVal = ref(false)
 <template>
   <CardBox>
     <BaseLevel type="justify-around lg:justify-center">
+      <div @click="openModal" class="cursor-pointer transition-transform hover:scale-105" title="Klik untuk memperbesar">
       <UserAvatarCurrentUser class="lg:mx-12" />
+      </div>
       <div class="space-y-3 text-center md:text-left lg:mx-12">
         <div class="flex justify-center md:block">
           <FormCheckRadio
@@ -40,4 +53,18 @@ const userSwitchVal = ref(false)
       </div>
     </BaseLevel>
   </CardBox>
+  <div v-if="isModalActive" class="fixed inset-0 z-50 flex items-center justify-center bg-black/80 backdrop-blur-sm p-4" @click.self="closeModal">
+    
+    <button @click="closeModal" class="absolute top-5 right-5 text-white hover:text-gray-300 transition">
+      <BaseIcon :path="mdiClose" size="36" />
+    </button>
+
+    <div class="relative max-w-3xl w-full">
+      <img 
+        :src="userAvatar" 
+        alt="Full Avatar" 
+        class="w-full h-auto rounded-lg shadow-2xl border-4 border-white/20"
+      >
+    </div>
+  </div>
 </template>
