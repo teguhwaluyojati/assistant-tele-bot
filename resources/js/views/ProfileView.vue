665461f7a55e890c06088ffb2d629bbded675b93
teguhwaluyojati <teguhwaluyojati14@gmail.com>
2025-11-22 15:26:21 +0700

    Creating Loading Component

--- resources/js/views/ProfileView.vue
+++ resources/js/views/ProfileView.vue
@@ -33,7 +33,12 @@ const handleFileUpload = (event) => {
   avatarFile.value = event.target.files[0];
 };
 
+const isLoading = ref(false);
+
 const submitProfile = async () => {
+  
+  isLoading.value = true;
+
   try {
     const formData = new FormData()
     
@@ -57,8 +62,6 @@ const submitProfile = async () => {
     mainStore.setUser(response.data.user)
 
     localStorage.setItem('user', JSON.stringify(response.data.user))
-
-    alert('Profile updated successfully!')
     
     avatarFile.value = null
 
@@ -74,10 +77,15 @@ const submitProfile = async () => {
       }
     }
     alert(errorMessage)
+  } finally {
+    isLoading.value = false;
   }
 }
 
 const submitPass = async() => {
+
+  isLoading.value = true;
+
   try{
     const response = await axios.post('/api/change-password',{
       current_password: passwordForm.password_current,
@@ -87,7 +95,6 @@ const submitPass = async() => {
 
     console.log('Password changed successfully:', response.data)
 
-    alert('Password changed successfully!')
   }catch(error){
     console.error('Error changing password:', error)
     let errorMessage = 'Something went wrong while changing password.';
@@ -99,6 +106,8 @@ const submitPass = async() => {
       }
     }
     alert(errorMessage);
+  } finally{
+    isLoading.value = false;
   }
 }
 </script>
@@ -195,4 +204,16 @@ const submitPass = async() => {
       </div>
     </SectionMain>
   </LayoutAuthenticated>
+  <div v-if="isLoading" class="fixed inset-0 z-50 flex items-center justify-center bg-slate-900/50 backdrop-blur-sm cursor-wait">
+      <div class="bg-white dark:bg-slate-800 p-6 rounded-lg shadow-xl flex flex-col items-center transform transition-all scale-100">
+        
+        <svg class="animate-spin h-10 w-10 text-blue-500 mb-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+          <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+        
+        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Processing...</h3>
+        <p class="text-sm text-gray-500 dark:text-gray-400">Please wait</p>
+      </div>
+    </div>
 </template>
