922f772e9a5477c6fdffbd863964e934e03f6582
teguhwaluyojati <teguhwaluyojati14@gmail.com>
2025-12-17 08:55:09 +0700

    Add Login to Web Route

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ Route::get('/', function () {
     return view('login');
 });
 
+Route::get('/login', function () {
+    return view('login');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 });
